8319343bf2fe57ab5238140325f28fb4d9a126a4
shicks <shicks255@yahoo.com>
2020-05-17 00:34:54 -0500

    UI improvements for review component

--- App/steves_reviews/src/components/Review.jsx
+++ App/steves_reviews/src/components/Review.jsx
@@ -5,7 +5,6 @@ import NoImage from '../images/no-album-cover.png';
 
 export default function Review(props) {
     const {review, album, artist, rating} = props;
-    const [collapsed, setCollapsed] = useState(true);
     const [image, setImage] = useState(NoImage);
 
     async function getImage(id) {
@@ -18,20 +17,28 @@ export default function Review(props) {
         getImage(album.id);
     }, []);
 
+    function toggleCollapse(e) {
+        e.preventDefault();
+        e.target.classList.toggle('fa-rotate-270');
+
+        const textarea = document.getElementById(`reviewContent_${review.id}`);
+        textarea.classList.toggle('collapsed');
+    }
+
     let imageStuff = '';
     let contentColumnClass = 'is-full';
     if (!props.hideAlbum) {
         contentColumnClass = 'is-three-quarters';
         imageStuff =
-            <div className="column is-one-quarter">
-            <Link to={`/artist/${artist.id}`}><b>{artist.name}</b></Link>
-            <figure className="image is-128x128">
-                <Link to={`/album/${album.id}`}>
-                    <img alt='myimage' src={image}/>
-                </Link>
-            </figure>
-            <Link to={`/album/${album.id}`}>{album.title}</Link>
-        </div>
+            <div className="column is-one-quarter has-text-left">
+                <Link to={`/artist/${artist.id}`}><b>{artist.name}</b></Link>
+                <figure className="image is-128x128">
+                    <Link to={`/album/${album.id}`}>
+                        <img alt='myimage' src={image}/>
+                    </Link>
+                </figure>
+                <Link to={`/album/${album.id}`}>{album.title}</Link>
+            </div>
     }
 
     return(
@@ -39,7 +46,7 @@ export default function Review(props) {
             <div className="columns noPad">
                 {imageStuff}
                 <div className={`column ${contentColumnClass}`}>
-                    <div className=''>
+                    <div className='has-text-left'>
                         <Link to={`user/${review.user.id}`}>{review.user.username}</Link>
                         <br/>
                         <span className={`tag ${review.colorClass}`}>
@@ -50,10 +57,11 @@ export default function Review(props) {
                     </div>
                     <br/>
                     <div>
-                        <span className="icon" id="reviewButton_@r.id" style={{cursor: 'pointer'}}>
+                        <span className="icon is-pulled-left" id={`reviewArrow_${review.id}`} onClick={toggleCollapse} style={{cursor: 'pointer'}}>
                             <i className="fas fa-arrow-down"></i>
                         </span>
-                        <div className="reviewContent" id="reviewContent_@r.id">
+                        <br/>
+                        <div className="reviewContent has-text-justified" id={`reviewContent_${review.id}`}>
                             {review.content}
                         </div>
                     </div>
