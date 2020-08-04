2d61545cb4d442f946f9323937248c2683594e80
shicks <shicks255@yahoo.com>
2020-08-03 23:02:41 -0500

    more metrics

--- src/main/java/com/steven/hicks/controllers/AlbumController.java
+++ src/main/java/com/steven/hicks/controllers/AlbumController.java
@@ -6,6 +6,9 @@ import com.steven.hicks.models.dtos.AlbumArtistReviewsDTO;
 import com.steven.hicks.models.dtos.ReviewDTO;
 import com.steven.hicks.services.AlbumService;
 import com.steven.hicks.services.JwtTokenService;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -24,24 +28,37 @@ public class AlbumController {
     private AlbumService m_albumService;
     @Autowired
     private JwtTokenService m_jwtTokenService;
+    @Autowired
+    private MeterRegistry m_meterRegistry;
 
     @GetMapping("/artist/{artistMbid}")
+    @Timed()
     public List<Album> getAlbumsForArtist(@PathVariable String artistMbid) {
         List<Album> albums = m_albumService.getAlbumsByArtist(artistMbid);
         return albums;
     }
 
     @GetMapping("/{albumMbid}")
+    @Timed()
     public AlbumArtistReviewsDTO getAlbum(@PathVariable String albumMbid, ServletRequest request) throws Exception {
         User user = null;
         try {
             user = m_jwtTokenService.getUserFromToken((HttpServletRequest) request);
         } catch (Exception e) {
         }
-        return m_albumService.getAlbumReviewArtistDTO(albumMbid, user);
+
+        AlbumArtistReviewsDTO dto = m_albumService.getAlbumReviewArtistDTO(albumMbid, user);
+        m_meterRegistry.counter("albumHits",
+                Arrays.asList(
+                        Tag.of("artist", dto.getArtist().getName()),
+                        Tag.of("album", dto.getAlbum().getTitle())
+                )
+        ).increment();
+        return dto;
     }
 
     @GetMapping("/topRated")
+    @Timed
     public List<ReviewDTO> getTopRated() {
         return m_albumService.getTopRated();
     }
--- src/main/java/com/steven/hicks/controllers/ArtistController.java
+++ src/main/java/com/steven/hicks/controllers/ArtistController.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.steven.hicks.logic.musicBrainz.MBArtistSearcher;
 import com.steven.hicks.models.artist.Artist;
 import com.steven.hicks.services.ArtistService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/artist")
@@ -18,17 +21,34 @@ public class ArtistController {
     @Autowired
     ArtistService m_artistService;
 
+    @Autowired
+    MeterRegistry m_meterRegistry;
+
     private MBArtistSearcher m_mbArtistSearcher = new MBArtistSearcher();
 
     @GetMapping("/{id}")
     public Artist getArtist(@PathVariable("id") String id) {
         Artist artist = m_artistService.getArtist(id);
         artist.getImages();
+
+        m_meterRegistry.counter("artistHits",
+                Arrays.asList(
+                        Tag.of("artist", artist.getName())
+                )
+        ).increment();
+
         return artist;
     }
 
     @GetMapping("/search/{searchTerms}")
     public JsonNode searchForArtist(@PathVariable String searchTerms) {
+
+        m_meterRegistry.counter("artistSearch",
+                Arrays.asList(
+                        Tag.of("searchTerms", searchTerms)
+                )
+        ).increment();
+
         JsonNode artistSearchResults = m_mbArtistSearcher.searchForArtist(searchTerms);
         return artistSearchResults;
     }
