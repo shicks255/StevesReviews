d91460364d74cd9b55aa95a95c99926b584975f5
shicks <shicks255@yahoo.com>
2020-08-18 22:18:41 -0500

    adding user and session id to the aspect logger

--- src/main/java/com/steven/hicks/aspects/OperationLogger.java
+++ src/main/java/com/steven/hicks/aspects/OperationLogger.java
@@ -1,13 +1,19 @@
 package com.steven.hicks.aspects;
 
+import com.steven.hicks.models.User;
+import org.apache.catalina.session.StandardSessionFacade;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -27,17 +33,32 @@ public class OperationLogger {
         logInfo.append("method={} ");
         logInfo.append("arguments={} ");
         logInfo.append("date={} ");
-        logInfo.append("time={}");
+        logInfo.append("time={} ");
+        logInfo.append("user={} ");
+        logInfo.append("sessionid={}");
+
+        String user = " ";
+        String session = " ";
+        StandardSessionFacade sessionFacade = (StandardSessionFacade)RequestContextHolder.currentRequestAttributes().getSessionMutex();
+        if (sessionFacade != null)
+            session = ((HttpSession) sessionFacade).getId();
+
+        SecurityContextImpl securityContext = (SecurityContextImpl)sessionFacade.getAttribute("SPRING_SECURITY_CONTEXT");
+        if (securityContext != null) {
+            Authentication authentication = securityContext.getAuthentication();
+            user = ((User)authentication.getPrincipal()).getUsername();
+        }
 
         Object[] args = new Object[]{
                 joinPoint.getSignature().getDeclaringType().getName(),
                 joinPoint.getSignature().getName(),
                 joinPoint.getArgs(),
                 LocalDate.now(),
-                LocalTime.now()
+                LocalTime.now(),
+                user,
+                session
         };
 
         logger.trace(logInfo.toString(), args);
-        System.out.println("executing advice");
     }
 }
